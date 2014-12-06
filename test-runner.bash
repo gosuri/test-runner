7f057939da4da799a30fcc633fad65897c96363c
Greg Osuri <gosuri@gmail.com>
2014-12-06 08:07:36 +0000

    Cleaned up output and added db-migrate-cmd and db-create-cmd options

--- test-runner.bash
+++ test-runner.bash
@@ -6,19 +6,7 @@ PROGRAM=${0##*/}
 RUBY_VERSION="2.1.2"
 NODE_VERSION="0.10.33"
 
-# Minimal set of packages necessary for running the application
-RUNTIME_PACKAGES="
-  zlib1g
-  libssl1.0.0
-  libreadline6
-  libyaml-0-2
-  sqlite3
-  libxml2
-  libxslt1.1
-  libcurl3
-"
-
-# Packages required to compile(install ruby and gems) the application
+# Packages required to compile (install ruby and gems) the application
 COMPILER_PACKAGES="
   unzip
   git
@@ -43,51 +31,36 @@ COMPILER_PACKAGES="
 "
 
 function run() {
+  trap cleanup EXIT
+  
   # setup git-ssh and copy private key to the cache directory
   setup_ssh
 
-  # build base container image if it doesn't exit
-  app_base_container_exists           || build_app_base_container
+  # setup base, ruby and app containers if they doesn't exit
+  setup_base_containers
   
-  # build base ruby container image with compilers if it doesn't exit
-  ruby_compiler_base_container_exists || build_ruby_compiler_base_container
-  
-  # fetch the new app code
-  get_app_source
-  
-  # build application development image with compilers if it doesn't exit
-  app_dev_container_exists            || build_app_dev_container
-
-  # compile application
+  # Compile application
   compile_app
 
-  # start support services (pg and redis)
+  # Start support services (pg and redis)
   start_services
   
-  trap finish EXIT
-  
-  # run tests
+  # Run tests
   run_tests
-}
-
-function finish() {
-  exitstatus=$?
   
-  # stop support services
-  stop_services
-  
-  # tag the current build as deploy_branch
-  if [ "${exitstatus}" == "0" ]; then
-    info "build successful"
+  # Tag the current build as deploy_branch
+  if [ $? -eq 0 ]; then
+    info "Build successful"
     tag_build
+    exit 0
   else
-    echo >&2 -e "build failed"
+    echo >&2 -e "Build failed"
+    exit $?
   fi
-
-  exit ${exitstatus}
 }
 
 function setup_ssh() {
+  log "Copying SSH keys. Using SSH private key from ${sshkey}"
   cat > "${cachedir}/git-ssh" <<EOF
   ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \$*
 EOF
@@ -99,8 +72,29 @@ EOF
  cp ${sshkey} ${cachedir}/app-base
 }
 
-function app_base_container_exists() {
-  test -n "$(docker images | grep '^app-base ')"
+
+function setup_base_containers() {
+  info "Setting up base containers"
+
+  # setup base container if it doesn't exist
+  if [ -n "$(docker images | grep '^app-base ')" ]; then
+    log "Using app-base container from cache"
+  else
+    build_app_base_container
+  fi
+
+  # setup base ruby compiler container if it doens't exists
+  if [ -n "$(docker images | grep '^ruby-compiler-base ')" ]; then
+    log "Using ruby-compiler-base container from cache"
+  else
+    build_ruby_compiler_base_container
+  fi
+  
+  if [ -n "$(docker images | grep "^${devimg} ")" ]; then
+    log "Using ${devimg} container from cache"
+  else
+    build_app_dev_container
+  fi
 }
 
 function build_app_base_container() {
@@ -129,23 +123,8 @@ EOF
   docker build --tag app-base ${cachedir}/app-base >> ${logfile} 2>&1
 }
 
-function get_app_source() {
-  local dir="${cachedir}/${devimg}/app"
-  
-  # keep it fresh
-  rm -rf ${dir} 
-  mkdir -p ${dir}
-
-  GIT_SSH="${cachedir}/git-ssh" git clone -b ${branch} ${repo} ${dir} --depth 1 >> ${logfile} 2>&1
-  log "source cloned to ${dir}"
-}
-
-function ruby_compiler_base_container_exists() {
-  test -n "$(docker images | grep '^ruby-compiler-base ')"
-}
-
 function build_ruby_compiler_base_container() {
-  log "building base ruby compiler container"
+  log "building ruby-compiler-base container"
   local dir="${cachedir}/ruby-compiler-base"
   local pkgs=$(printf "%s " $COMPILER_PACKAGES)
 
@@ -174,10 +153,6 @@ EOF
   docker build --tag ruby-compiler-base ${dir} >> ${logfile} 2>&1
 }
 
-function app_dev_container_exists() {
-  test -n "$(docker images | grep "^${devimg} ")"
-}
-
 function build_app_dev_container() {
   log "building ${devimg} container"
   local dir="${cachedir}/${devimg}"
@@ -192,7 +167,8 @@ EOF
 }
 
 function compile_app() {
-  info "compiling application"
+  info "Compiling application"
+  get_app_source
   local dir="${cachedir}/${devimg}"
   mkdir -p ${dir}
   cat > "${dir}/Dockerfile" <<EOF
@@ -204,30 +180,23 @@ EOF
   docker build --tag ${devimg} $dir >> ${logfile} 2>&1
 }
 
-function run_tests() {
-  log "creating db"
-  docker run --env-file=${envfile}  $devimg bundle exec rake db:create
-  
-  log "loading schema"
-  docker run --env-file=${envfile} $devimg bundle exec rake db:schema:load
-
-  info "running testing using bundle exec rspec"
-  docker run --env-file=${envfile} $devimg bundle exec rspec
-}
-
-function tag_build() {
+function get_app_source() {
+  log "Fetching application source from ${repo} on branch ${branch}"
   local dir="${cachedir}/${devimg}/app"
-  (cd $dir && git tag -f "deploy_${branch}" && git push -f origin "deploy_${branch}")
+  rm -rf ${dir} # keep it fresh
+  mkdir -p ${dir}
+  GIT_SSH="${cachedir}/git-ssh" git clone -b ${branch} ${repo} ${dir} --depth 1 >> ${logfile} 2>&1
 }
 
 function start_services() {
-  redis=$(docker run --name $devimg-redis-${RANDOM} -d redis)
-  log "running redis container"
+  info "Staring support services"
+  redis=$(docker run --name $devimg-redis-${RANDOM} -d redis 2> ${logfile})
   redisip=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" ${redis})
+  log "running redis container at ${redisip}:6379"
   
   pg=$(docker run --name $devimg-pg-${RANDOM} -d postgres)
   pgip=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" ${pg})
-  log "running postgres"
+  log "running postgres container at ${pgip}:5432"
 
   envfile="${cachedir}/.env"
 
@@ -237,127 +206,40 @@ POSTGRES_HOST=${pgip}
 EOF
 }
 
-function stop_services() {
+function run_tests() {
+  log "Creating database using: ${dbcreatecmd}"
+  docker run --env-file=${envfile} $devimg ${dbcreatecmd} >> ${logfile} 2>&1
+
+  log "Running database migrations using: ${dbmigratecmd}"
+  docker run --env-file=${envfile} $devimg ${dbmigratecmd} >> ${logfile} 2>&1
+
+  info "Running tests using: ${testcmd}"
+  docker run --env-file=${envfile} -t $devimg ${testcmd}
+}
+
+function tag_build() {
+  local dir="${cachedir}/${devimg}/app"
+  local tag="deploy_${branch}"
+  log "Pushing tag ${tag} to ${repo}"
+  (cd $dir && git tag -f ${tag} && GIT_SSH="${cachedir}/git-ssh" git push -f origin ${tag} >> ${logfile} 2>&1)
+}
+
+function cleanup() {
+  info "Cleaning up"
+  exitstatus=$?
+  
+  # stop support services
   if [ -n ${redis} ]; then
-    docker rm -f ${redis}
-    log "removed redis container"
+    docker rm -f ${redis} >> ${logfile} 2>&1
+    log "Removed redis container"
   fi
 
   if [ -n ${pg} ]; then
-    docker rm -f ${pg}
-    log "removed postgres container"
+    docker rm -f ${pg} >> ${logfile} 2>&1
+    log "Removed postgres container"
   fi
-}
-
-function copy_compiled_app() {
-  local id=$(docker run -dt app-compiler /bin/bash)
-  local dir=${cachedir}/app-runtime
-  mkdir -p ${dir}
-  docker cp ${id}:/app ${dir}
-  log "application compiled to ${dir}"
-  docker rm --force ${id} > /dev/null
-  log "removing compiler copy container ${id}"
-}
-
-function app_runtime_container_exists() {
-  test -n "$(docker images | grep '^app-runtime ')"
-}
-
-function build_app_runtime_container() {
- log "building app runtime base container"
- local secretsbase=$(date +%s | sha256sum | base64 | head -c 64)
- local pkgs=$(printf "%s " $RUNTIME_PACKAGES)
- local dir="${cachedir}/app-runtime"
- tee "${dir}/Dockerfile" > /dev/null <<EOF
-FROM app-base
-RUN apt-get update && apt-get install -y ${pkgs}
-ENV RAILS_ENV production
-ENV SECRET_KEY_BASE ${secretsbase}
-RUN mkdir -p /app
-EOF
-  docker build --tag app-runtime ${dir} >> ${logfile} 2>&1
-}
-
-function build_runtime_image() {
-  info "building app-runtime container image"
-  dir="${cachedir}/app-runtime"
-  mkdir -p ${dir}
-  cat > ${dir}/Dockerfile <<EOF
-FROM app-runtime
-ADD app app
-WORKDIR /app
-EOF
-  docker build --tag app-runtime ${dir} >> ${logfile} 2>&1
-  info "application successfully built"
-}
-
-function info() {
-  local msg="--> ${PROGRAM}: ${*}"
-  echo -e "${msg}" >> ${logfile}
-  echo -e "${msg}"
-}
-
-function log() {
-  local msg="${*}"
-
-  if [ ${verbose} -gt 0 ]; then
-    echo -e "${msg}"
-  fi
-  echo -e "${msg}" >> ${logfile}
-}
-
-
-function show_help() {
-  cat <<EOF
-Usage: ${PROGRAM} [OPTIONS] GIT_REPO
-
-Runs tests for the given repo in a container
-
-Options:
-  -b, --branch="master"               Git branch
-  -k, --ssh-key="\$HOME/.ssh/id_rsa"   SSH key for git repo access
-  -a, --app-name=""                   Application name. Autogenerated based on the git repo path
-  -c, --cache-dir=""                  Cache directory
-  -v, --verbose
-EOF
-}
-
-function parse_opts() {
-  repo="${@: -1}"
-
-  while [[ $# > 0 ]]; do
-    local key="$1"
-    shift
-    case ${key} in
-      -b|--branch=)
-        branch="${1}"
-        shift
-        ;;
-      -c|--cache-dir=)
-        cachedir="${1}"
-        shift
-        ;;
-      -k|--ssh-key=)
-        sshkey="${1}"
-        shift
-        ;;
-      -a|--app-name=)
-        appname="${1}"
-        shift
-        ;;
-      -h|--help)
-        show_help
-        exit 0
-        ;;
-      -v|--verbose)
-        verbose=1
-        ;;
-    esac
-  done
-  if [ -z "${repo}" ]; then
-    show_help 
-    exit 1
-  fi
+  
+  exit ${exitstatus}
 }
 
 function init() {
@@ -385,7 +267,8 @@ function init() {
   
   logfile="${cachedir}/run.log"
 
-  info "running tests for ${appname} from ${repo}. More info ${logfile}"
+  info "Starting tests for ${appname}"
+  log "Logging to ${logfile}"
   
   # exit script if we would use an uninitialised variable
   set -o nounset
@@ -410,6 +293,94 @@ function abort_if_missing_command() {
   type ${cmd} >/dev/null 2>&1 || abort "${2}"
 }
 
+function info() {
+  local msg="==> ${PROGRAM}: ${*}"
+  local bold=$(tput bold)
+  local reset=$(tput sgr0)
+  echo -e "${msg}" >> ${logfile}
+  echo -e "${bold}${msg}${reset}"
+}
+
+function log() {
+  local msg="    ${PROGRAM}: ${*}"
+  echo -e "${msg}"
+  echo -e "${*}" >> ${logfile}
+}
+
+function debug() {
+  local msg="    [debug] ${*}"
+  if [ ${verbose} -gt 0 ]; then
+    echo -e "${msg}"
+  fi
+  echo -e "${msg}" >> ${logfile}
+}
+
+function show_help() {
+  cat <<EOF
+Usage: ${PROGRAM} [OPTIONS] GIT_REPO
+
+Runs tests for the given repo in a container
+
+Options:
+  -b, --branch="master"                 Git branch
+  -k, --ssh-key="\$HOME/.ssh/id_rsa"     SSH key for git repo access
+  -a, --app-name=""                     Application name. Autogenerated based on the git repo path
+  -c, --cache-dir=""                    Cache directory
+  -t, --test-cmd="${testcmd}"    Test command running tests
+  -v, --verbose
+EOF
+}
+
+function parse_opts() {
+  repo="${@: -1}"
+
+  while [[ $# > 0 ]]; do
+    local key="$1"
+    shift
+    case ${key} in
+      -b|--branch=)
+        branch="${1}"
+        shift
+        ;;
+      -c|--cache-dir=)
+        cachedir="${1}"
+        shift
+        ;;
+      -k|--ssh-key=)
+        sshkey="${1}"
+        shift
+        ;;
+      -a|--app-name=)
+        appname="${1}"
+        shift
+        ;;
+      -t|--test-cmd=)
+        testcmd="${1}"
+        shift
+        ;;
+      -d|--db-create-cmd=)
+        dbcreatecmd="${1}"
+        shift
+        ;;
+      -m|--db-migrate-cmd=)
+        dbmigratecmd="${1}"
+        shift
+        ;;
+      -h|--help)
+        show_help
+        exit 0
+        ;;
+      -v|--verbose)
+        verbose=1
+        ;;
+    esac
+  done
+  if [ -z "${repo}" ]; then
+    show_help 
+    exit 1
+  fi
+}
+
 verbose=0
 repo=''
 cachedir=''
@@ -418,6 +389,13 @@ logfile=''
 devimg=''
 sshkey="${HOME}/.ssh/id_rsa"
 branch='master'
+testcmd="bundle exec rspec"
+dbcreatecmd="bundle exec rake db:create"
+dbmigratecmd="bundle exec rake db:schema:load"
+redis=''
+redisip=''
+pg=''
+pgip=''
 
 parse_opts $*
 init
